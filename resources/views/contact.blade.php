5f26eeb549d28e272b474c312c8c19412b1b7fae
CDulst <casey.dulst@gmail.com>
2021-07-14 09:36:47 +0200

    css variables + typography/button styling

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.typekit.net/cro7cdq.css">
+    <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>
-
+<input class = "form--button" type="submit" value = "verzenden"/>
 </body>
 </html>

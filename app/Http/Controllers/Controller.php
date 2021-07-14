a1591fac6286b5eac03215ab0912331a0b77ecbc
CDulst <casey.dulst@gmail.com>
2021-07-14 09:20:35 +0200

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Session;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function contact()
+    {
+
+    }
+}
